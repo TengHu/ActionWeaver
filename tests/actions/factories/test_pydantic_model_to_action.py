50a716970786559bf20ccfc7d3cdf899f16f4b93
Niel Teng Hu <10646097+TengHu@users.noreply.github.com>
2023-12-20 09:55:55 -0500

    add decorators to action_from_model (#101)

--- tests/actions/factories/test_pydantic_model_to_action.py
+++ tests/actions/factories/test_pydantic_model_to_action.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import functools
 import unittest
 from typing import List
 
@@ -56,6 +57,84 @@ class TestCase(unittest.TestCase):
                 places=[TestCase.Place(lat=23.0, lng=23.0, description="description")]
             ),
         )
+
+    def test_from_pydantic_model_with_decorators(self):
+        def decorator(func):
+            @functools.wraps(func)
+            def duplicate(*args, **kwargs):
+                result = func(*args, **kwargs)
+                return result, result
+
+            return duplicate
+
+        action = action_from_model(
+            TestCase.Place, name="Func1", description="Extract Place model"
+        )
+        self.assertTrue("create_place" in action.user_method.__name__)
+        self.assertEqual(action.name, "Func1")
+        self.assertEqual(action.stop, True)
+        self.assertEqual(action.description, "Extract Place model")
+        self.assertEqual(
+            action(**{"place": {"lat": 23, "lng": 23, "description": "description"}}),
+            TestCase.Place(lat=23.0, lng=23.0, description="description"),
+        )
+
+        action = action_from_model(
+            TestCase.Places,
+            name="Func2",
+            description="Extract Places model",
+            decorators=[decorator],
+        )
+
+        self.assertTrue("create_place" in action.user_method.__name__)
+        self.assertEqual(len(action.decorators), 1)
+
+        self.assertEqual(action.name, "Func2")
+        self.assertEqual(action.stop, True)
+        self.assertEqual(action.description, "Extract Places model")
+
+        actual = action(
+            **{
+                "places": {
+                    "places": [{"lat": 23, "lng": 23, "description": "description"}]
+                }
+            }
+        )
+
+        # This test verify that the decorator is not incorporated into the pydantic model
+        self.assertTrue(
+            action.json_schema()["properties"], {"places": {"$ref": "#/$defs/Places"}}
+        )
+        self.assertEqual(
+            action.json_schema()["$defs"],
+            {
+                "Place": {
+                    "description": "A geo location",
+                    "properties": {
+                        "description": {"title": "Description", "type": "string"},
+                        "lat": {"title": "Lat", "type": "number"},
+                        "lng": {"title": "Lng", "type": "number"},
+                    },
+                    "required": ["lat", "lng", "description"],
+                    "title": "Place",
+                    "type": "object",
+                },
+                "Places": {
+                    "description": "A geo location",
+                    "properties": {
+                        "places": {
+                            "items": {"$ref": "#/$defs/Place"},
+                            "title": "Places",
+                            "type": "array",
+                        }
+                    },
+                    "required": ["places"],
+                    "title": "Places",
+                    "type": "object",
+                },
+            },
+        )
+        self.assertEqual(len(actual), 2)
 
 
 if __name__ == "__main__":
