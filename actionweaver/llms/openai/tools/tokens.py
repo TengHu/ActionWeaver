88c0507e81a360faea4fd2145398dbe988c257a5
Niel Teng Hu <hu.niel92@gmail.com>
2023-12-29 18:31:59 -0500

    add logging into chat and action (#108)

--- actionweaver/llms/openai/tools/tokens.py
+++ actionweaver/llms/openai/tools/tokens.py
@@ -9,8 +9,7 @@ class TokenUsageTrackerException(Exception):
 
 
 class TokenUsageTracker:
-    def __init__(self, budget=None, logger=None):
-        self.logger = logger
+    def __init__(self, budget=None):
         self.tracker = CompletionUsage(
             completion_tokens=0, prompt_tokens=0, total_tokens=0
         )
@@ -29,28 +28,7 @@ class TokenUsageTracker:
             total_tokens=self.tracker.total_tokens + usage.total_tokens,
         )
 
-        if self.logger:
-            self.logger.debug(
-                {
-                    "message": "token usage updated",
-                    "usage": usage,
-                    "total_usage": dict(self.tracker),
-                    "timestamp": time.time(),
-                    "budget": self.budget,
-                },
-            )
-
         if self.budget is not None and self.tracker.total_tokens > self.budget:
-            if self.logger:
-                self.logger.error(
-                    {
-                        "message": "Token budget exceeded",
-                        "usage": usage,
-                        "total_usage": self.tracker,
-                        "budget": self.budget,
-                    },
-                    exc_info=True,
-                )
             raise TokenUsageTrackerException(
                 f"Token budget exceeded. Budget: {self.budget}, Usage: {self.tracker}"
             )
